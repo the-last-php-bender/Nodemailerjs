fd909b30dfc1126aa6287152a1f9f7b98a36b861
Dayveed12345 <nwinyinyadavid123@gmail.com>
2023-05-31 21:52:45 +0100

    I Added the public folder

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
-const mailer=require('nodemailer');
-const transport =mailer.createTransport({
-    host:'smtp.gmail.com',
-    auth:{
-        user:'nwinyinyadavid123@gmail.com',
-        pass:'shptzbeewflfjkjw'
-    }
+const express=require("express");
+const app=express();
+const nodemailer= require("nodemailer");
+const PORT=process.env.PORT||5000;
+app.use(express.static('public'));
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/public/contact.html');
 })
-const mailOptions={
-from :'nwinyinyadavid123@gmail.com',
-to:'dayveed070@gmail.com',
-subject:'nodemailer',
-text:'Yes it is working'
-}
-transport.sendMail(mailOptions,function(err,info){
-if(err){
-    console.log(err);
-}else{
-    console.log(info.response);
-}
+app.post('/',(req,res)=>{
+    console.group(req.body)
+    const transporter= nodemailer.createTransport({
+       host:"smtp.gmail.com",
+        auth:{
+            user:"nwinyinyadavid123@gmail.com",
+            pass:"shptzbeewflfjkjw"
+        }
+    })
+    const mailOptions = {
+        from:"nwinyinyadavid123@gmail.com",
+        to:req.body.email,
+        subject:`Message for ${req.body.email}:  ${req.body.subject}`,
+        text:req.body.message
+    }
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent:'+info.response)
+            res.send('success')
+        }
+    })
+})
+app.listen(PORT, ()=>{
+    console.log(`Running on port: ${PORT}`)
 })
